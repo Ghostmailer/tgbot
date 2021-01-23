d239aa8f28dd18e8dc5fa19da3341cd11f92f3a7
Ghostmailer <72339064+Ghostmailer@users.noreply.github.com>
2021-01-23 11:49:59 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -140,7 +140,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton(text="💰 Join & Earn 💰", url="https://t.me/orionclubprogroup"),  InlineKeyboardButton(text="💰 Register & Earn 💰", url="https://orionclubs.com/#/register?r_code=3804")],
                      [InlineKeyboardButton(text="💰 Join For Updates 💰", url="https://t.me/orionclub3804Progress")],
-                     [InlineKeyboardButton(text="🛠 My DEV", url="https://t.me/{}?start=help".format(bot.username)) ]]))
+                     [InlineKeyboardButton(text="🛠 My DEV", url="https://t.me/physic_hybrid) ]]))
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
 
